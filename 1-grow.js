3cc3ff7d7fa4aec2a3f8e78ba236fc7a86e90f1c
gustiaryapriandana31 <gustiaryapriandana@gmail.com>
2024-02-03 12:13:36 +0700

    update desc for Day @1/366

--- 1-grow.js
+++ 1-grow.js
@@ -1,3 +1,9 @@
+// Coding Challenge 2024 - Inspired by WPU
+// 1/366
+// https://www.codewars.com/kata/57f780909f7e8e3183000078
+// 8 kyu
+
+
 // function grow(x) {
 //   let result = x[0];
 //   for (let i = 1; i < x.length; i++) {
@@ -10,7 +16,6 @@
 //     return x.reduce((acc, cur) => acc * cur)
 // }
 
+const grow = (x) => x.reduce((acc, cur) => acc * cur);
 
-const grow = x => x.reduce((acc, cur) => acc * cur)
-
-console.log(grow([1,2,3,3]));
+console.log(grow([1, 2, 3, 3]));
